ad53f5420a483f659117435c8ce00950dbb664d6
Epa <epa@epaneto.com>
2014-01-26 11:17:02 -0200

    plant controller type

--- sources-sparrow/PeeQuest/PeeQuest/obstacles/PQPlantController.m
+++ sources-sparrow/PeeQuest/PeeQuest/obstacles/PQPlantController.m
@@ -11,6 +11,9 @@
 @property BOOL isHide;
 @property SPMovieClip * assetView;
 @property SPMovieClip * shadowView;
+@property SPTextureAtlas * atlas;
+@property (nonatomic,strong) NSArray *asset_textures;
+@property (nonatomic,strong) NSArray *shadow_textures;
 @end
 
 @implementation PQPlantController
@@ -23,18 +26,64 @@
 {
     _isHide = !_isHide;
     
+    [self clearAnimations];
+    
     if(_isHide)
     {
-        
+        [self showAssetAnimation:@"vaso/idle_"];
     }else{
         
     }
 }
 
--(void)showAssetAnimation :(NSString * )
+-(void)setup
 {
-    [self.container removeAllChildren];
+    NSString * path = [NSString stringWithFormat:@"%@.xml", self.assestName];
     
+    // initialize player atlas
+    _atlas = [SPTextureAtlas atlasWithContentsOfFile:path];
+    
+    [self showAssetAnimation:@"vaso/idle_"];
+
+}
+
+-(void)showAssetAnimation :(NSString * )label
+{
+    _asset_textures = [_atlas texturesStartingWith:label];
+    
+    _assetView = [[SPMovieClip alloc] initWithFrames:_asset_textures fps:30];
+    _assetView.loop = YES;
+    [Sparrow.juggler addObject:_assetView];
+    [_assetView play];
+    
+    [self.container addChild:_assetView];
+}
+
+-(void)showShadowAnimation :(NSString * )label
+{
+    _shadow_textures = [_atlas texturesStartingWith:label];
+    
+    _shadowView = [[SPMovieClip alloc] initWithFrames:_shadow_textures fps:30];
+    _shadowView.loop = YES;
+    [Sparrow.juggler addObject:_shadowView];
+    [_shadowView play];
+    
+    [self.container addChild:_shadowView];
+}
+
+-(void)clearAnimations
+{
+    if(_assetView)
+    {
+        [Sparrow.juggler removeObject:_assetView];
+        [self.container removeChild:_assetView];
+    }
+    
+    if(_shadowView)
+    {
+        [Sparrow.juggler removeObject:_shadowView];
+        [self.container removeChild:_shadowView];
+    }
 }
 
 @end
